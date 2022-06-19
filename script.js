58ed9733df6c1d378a2fd2e4cc7e2abf6b598865
samuelconlon <conlonsp@yahoo.com>
2022-06-19 12:39:59 -0400

    added img link to index.html, styled <a> in style.css, update h2 in script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+h2.textContent = "My name is Sam!"
+h2.style.textAlign = "center";
+h2.style.fontSize = "50px";
